844d5af469b450b1a3c42b8e81ca5fee5a2b7dfd
Boren Zhang <bobo1993324@gmail.com>
2014-12-07 14:09:16 -0500

    fix install

--- install.sh
+++ install.sh
@@ -11,5 +11,6 @@ BIN_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 LIB_DIR=$BIN_DIR/../lib
 
 sed s/impala_shell.py/blinkdb_shell.py/ < $BIN_DIR/impala-shell > $BIN_DIR/blinkdb-shell
+chmod +x $BIN_DIR/blinkdb-shell
 cp shell/blinkdb_shell.py $LIB_DIR/impala-shell
 ln -s $BIN_DIR/blinkdb-shell /usr/bin
